5439a968ff2019b36fb42391ec17d9a4e8f76a5f
CastroFR <kevinfrancisco906@gmail.com>
2025-09-23 16:54:03 -0600

    CLASE 22/09/25 | BinaryThree explicación y reto en clase

--- PHP/estructuras_datos.php
+++ PHP/estructuras_datos.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
 //Arrays
 //Declaracion
 //Array indexados
-$array = [1,2,3,4,5];
+$array = [1, 2, 3, 4, 5];
 $array2 = array();
 $array3 = new ArrayObject();
 
@@ -19,7 +19,7 @@ print_r($arrayAsociativo["nombre"] . "\n");
 //Array propiedades y metodos
 
 //Saber el largo del array
-print count($array)."\n";
+print count($array) . "\n";
 
 //Agregar un elemento al final del array
 array_push($array, 6);
@@ -39,7 +39,7 @@ array_shift($array);
 print_r($array);
 
 //Recorrer un array
-foreach($array as $valor){
+foreach ($array as $valor) {
     echo "Valor: {$valor}\n";
 }
 
@@ -47,7 +47,7 @@ foreach($array as $valor){
 $arrayMultimensional = [
     "nombre" => "Jairo",
     "edad" => 30,
-    "hobbies" => ["Programar", "Leer", "Correr", "otros" => [ "Jugar jueguitos" => ["LOL","DOTA2","CS2"]]]
+    "hobbies" => ["Programar", "Leer", "Correr", "otros" => ["Jugar jueguitos" => ["LOL", "DOTA2", "CS2"]]]
 
 ];
 
@@ -56,23 +56,27 @@ print_r($arrayMultimensional);
 
 //Clases y objetos 
 
-class Persona {
+class Persona
+{
     //Atributos o propiedades
     private $nombre;
     private $edad;
 
     //constructor
-    function __construct($nombreParam, $edadParam){
+    function __construct($nombreParam, $edadParam)
+    {
         $this->nombre = $nombreParam;
         $this->edad = $edadParam;
     }
 
     //Metodos o funciones
-    public function getNombre() {
+    public function getNombre()
+    {
         return $this->nombre;
     }
 
-    public function getEdad() {
+    public function getEdad()
+    {
         return $this->edad;
     }
 
@@ -80,7 +84,7 @@ class Persona {
      * Set the value of nombre
      * @param string $nombre
      * @return  self
-     */ 
+     */
     public function setNombre($nombre)
     {
         $this->nombre = $nombre;
@@ -92,17 +96,13 @@ class Persona {
      * Set the value of edad
      * @param int $edad
      * @return  self
-     */ 
+     */
     public function setEdad($edad)
     {
         $this->edad = $edad;
 
         return $this;
     }
-
-    
-
-	
 }
 
 //Crear un objeto
@@ -114,95 +114,120 @@ echo $persona1->getNombre() . "\n";
 
 //LIFO -> Stack -> Last In First Out
 
-class Stack {
+class Stack
+{
     private $data;
 
     //Constructor con parametros OPCIONALES
-    function __construct($dataParams = []){
+    function __construct($dataParams = [])
+    {
         $this->data = $dataParams;
     }
 
     //Agregar un elemento
-    function add($element){
+    function add($element)
+    {
         array_push($this->data, $element);
         //$this->data[] = $element;
     }
 
     //Eliminar un elemento
-    function remove(){
+    function remove()
+    {
         return array_pop($this->data);
     }
 }
 
-$stacito = new Stack([1,2,3,4]);
+$stacito = new Stack([1, 2, 3, 4]);
 
 
 //FIFO -> Queue -> First In First Out
 
-class Queue{
+class Queue
+{
     private $data;
 
-    function __construct($dataParams = []){
+    function __construct($dataParams = [])
+    {
         $this->data = $dataParams;
     }
 
     //Agregar un elemento
-    function add($element){
+    function add($element)
+    {
         array_push($this->data, $element);
     }
 
     //Eliminar un elemento
-    function remove(){
+    function remove()
+    {
         return array_shift($this->data);
     }
 }
 
-class Node{
+class Node
+{
     private $value;
     private $next;
 
-    function __construct($valueParam){
+    function __construct($valueParam)
+    {
         $this->value = $valueParam;
         $this->next = null;
     }
 
-    public function getValue() {
+    public function getValue()
+    {
         return $this->value;
     }
 
-    public function getNext() {
+    public function getNext()
+    {
         return $this->next;
     }
 
-    public function setNext($next) {
+    public function setNext($next)
+    {
         $this->next = $next;
     }
 }
 
-class LinkedList{
+class LinkedList
+{
     private $head;
 
-    function __construct(){
+    function __construct()
+    {
         $this->head = null;
     }
 
-    function add($value){
+    function add($value)
+    {
         //Creamos un nuevo nodo
         $newNode = new Node($value);
 
-        if($this->head === null){
+        if ($this->head === null) {
             $this->head = $newNode;
-        }else{
+        } else {
             $current = $this->head;
 
             //Recorremos la lista mientras el siguiente no sea nulo
-            while($current->getNext() !== null){
+            while ($current->getNext() !== null) {
                 $current = $current->getNext();
             }
             //Cuando encontramos el final, agregamos el nuevo nodo
             $current->setNext($newNode);
         }
     }
+
+    // ***************** RETO DIVIDIDO EN SALAS *****************
+
+
+    //Funcion para encontrar un dato. Devolveria un mensaje si este dato existe
+
+    //Funcion para eliminar
+
+
 }
 
 $listita = new LinkedList();
@@ -210,4 +235,107 @@ $listita->add(3);
 $listita->add(1);
 $listita->add(5);
 print_r($listita);
-?>
+
+
+class Nodo
+{
+    private $value;
+    private $left;
+    private $right;
+
+    function __construct($valueParam)
+    {
+        $this->value = $valueParam;
+        $this->left = null;
+        $this->right = null;
+    }
+
+    function getValue()
+    {
+        return $this->value;
+    }
+
+    function getRight()
+    {
+        return $this->right;
+    }
+
+    function getLeft()
+    {
+        return $this->left;
+    }
+
+    function setRight($data)
+    {
+        $this->right = $data;
+    }
+
+    function setLeft($data)
+    {
+        $this->left = $data;
+    }
+
+    function setValue($data)
+    {
+        $this->value = $data;
+    }
+}
+
+
+class BinaryThree
+{
+    private $root;
+
+    function __construct($data = null)
+    {
+        $this->root = $data;
+    }
+
+    function insert($data)
+    {
+        $newNode = new Nodo($data);
+
+        if ($this->root === null) {
+            $this->root = $newNode;
+            return $this->root;
+        }
+
+        $current = $this->root;
+
+        while (true) {
+
+            if ($newNode->getValue() > $current->getValue()) {
+                if ($current->getRight() === null) {
+                    $current->setRight($newNode);
+                    return $newNode;
+                } else {
+                    $current = $current->getRight();
+                }
+            } else {
+                if ($current->getLeft() === null) {
+                    $current->setLeft($newNode);
+                    return $newNode;
+                } else {
+                    $current = $current->getLeft();
+                }
+            }
+        }
+    }
+
+    // ***************** RETO DIVIDIDO EN SALAS *****************
+
+    //Funcion para encontrar un dato. Devolveria un mensaje si este dato existe
+
+    //Funcion para eliminar
+    
+}
+
+$nuevoNodo = new Nodo(10);
+
+$arbolito = new BinaryThree($nuevoNodo);
+print_r($arbolito);
+echo "\n";
+$arbolito->insert(17);
+$arbolito->insert(19);
+$arbolito->insert(13);
+print_r($arbolito);

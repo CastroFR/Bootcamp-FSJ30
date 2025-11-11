22e73bbf277246bb3c89abd9135e438a413c1369
Kevin Castro <kevinfrancisco906@gmail.com>
2025-11-11 15:35:14 -0600

    CLASE 10/11/25 - Continuación Proyecto Laravel | Petición PUT y Repaso retroalimentación

--- LARAVEL/first-project/app/Models/Product.php
+++ LARAVEL/first-project/app/Models/Product.php
@@ -3,13 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Product extends Model
 {
     //
-    protected $fillable = [
+    protected $fillable =[
         'name',
         'description',
-        'price',
+        'price'
     ];
+
+    //Ejemplo de consulta query builder    
+    public static function getAllProducts(){
+        return DB::table('products')->select('name','description','price')->get();
+    }
+
+    //Ejemplo de insercion query builder
+    public static function saveProduct($name, $description, $price){
+        return DB::table('products')->insert([
+            'name' => $name,
+            'description' => $description,
+            'price' => $price
+        ]);
+    }
+
+    
 }
+        
--- LARAVEL/first-project/routes/api.php
+++ LARAVEL/first-project/routes/api.php
@@ -13,6 +13,8 @@ Route::get('/user', function (Request $request) {
 //Route::get('/products', [App\Http\Controllers\ProductController::class, 'index']);
 //2da forma de utilizar el controlador
 // http://localhost:8000/api/products
-Route::get('/products', [ProductController::class, 'index']); //obtenemos datos
+Route::get('/products', [ProductController::class, 'index']);
 // http://localhost:8000/api/products
-Route::post('/products', [ProductController::class, 'store']); //enviamos datos
+Route::post('/products', [ProductController::class, 'store']);
+// http://localhost:8000/api/products/1
+Route::put('/products/{id}', [ProductController::class, 'update']);
